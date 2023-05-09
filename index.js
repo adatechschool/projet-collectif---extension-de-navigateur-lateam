362afdd97ddc1c676a3e4927e4f1837d84cf94a9
Laurefav <Laure.Favre28@laposte.net>
2023-05-09 15:59:13 +0200

    modif

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+async function books () {
+
+    const options = {
+        method: 'GET',
+        url: 'https://hapi-books.p.rapidapi.com/nominees/romance/2020',
+        headers: {
+          'X-RapidAPI-Key': 'fdbe3dd835msh650295f34ceb017p126015jsn52284abe399c',
+          'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com'
+        }
+      };
+
+    let response = await fetch ('https://hapi-books.p.rapidapi.com/nominees/romance/2020', options )
+    let result = await response.json();
+
+   
+    let bookname = result[0].name
+    
+    document.getElementById('books').innerHTML= bookname
+   
+    // for (let i = 0; i < result.length; i++) {
+    //     // const element = result[i][0];
+    //     console.log(result[i][0]); 
+    // }
+  
+    console.log(bookname)
+    return result;
+}
+
+books()
+
+
+
+
